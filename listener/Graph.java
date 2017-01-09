3ce990ba4b4e954b44adf42519135871f63fd692
Devin Conley <devinconley@jhu.edu>
2017-01-09 00:36:21 -0500

    Filled in constructor and reconfig

--- listener/Graph.java
+++ listener/Graph.java
@@ -2,33 +2,67 @@
 
 class Graph
 {
+    // Config
+    int posY;
+    int posX;
+    int height;
+    int width;
+    
+    boolean xvy;
+    int numVars;
+    int maxPoints;
+    String title;
+    String[] labels;
+
     // Data
-
-
+    int posX;
+    double[][] data;
+    int[] extremesCounter;
+    double[] extremes; // {min_x, max_x, min_y, max_y}
 
     // Contructor
-    public Graph( int posX, int posY, int height, int width,
+    public Graph( int posY, int posX, int height, int width,
 		  boolean xvy, int numVars, int maxPoints,
 		  String title, String[] labels )
     {
-
+	this.posY = posY;
+	this.posX = posX;
+	this.height = height;
+	this.width = width;
+	this.xvy = xvy;
+	this.numVars = numVars;
+	this.maxPoints = maxPoints;
+	this.title = title;
+	this.labels = labels;	
     }
 
     // Modifiers
-    public void Reconfigure()
-    {}
+    public void Reconfigure( int posY, int posX, int height, int width,
+			     boolean xvy, int numVars, int maxPoints,
+			     String title, String[] labels )
+    {
+	this.posY = posY;
+	this.posX = posX;
+	this.height = height;
+	this.width = width;
+	this.xvy = xvy;
+	this.numVars = numVars;
+	this.maxPoints = maxPoints;
+	this.title = title;
+	this.labels = labels;	
+    }
     
-    public void Update()
+    public void Update( double[] newData )
     {}
 
     public void Plot()
     {}
 
     // Internal Helpers
-    private void PlotXY()
+    private void DrawDataXY()
     {}
 
-    private void PlotTime()
+    private void DrawDataTime()
     {}
 
     private void DrawTicks()
